f91790b992cf4ce04ffb134818f699fbfcfaa858
laibamalik45 <laibamalik45d@gmail.com>
2023-09-24 04:01:44 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,195 @@
+// q no 1 
+//  for(var i = 0; i <=10; i++){
+//     document.write("hello world" + "<br>")
+//  }
+// q no 3
+// for (var i = 0; i <= 4 ; i++){
+//     console.log("hello world")
+// }
+//  q no 2
+// for(var i = 1; i<=12;i++){
+//     document.write("my name is laiba"+ "<br>")
+// }
+
+
+
+ //  qno 4
+//  for(var name = 1; name <=100; name++){
+//         document.write("hello world" + "<br>")
+//     }
+
+// q no 5
+// for(var i = 3; i>=1; i--){
+//     console.log(i);
+// }
+ 
+
+
+// q no 6
+// var name = [2];
+
+//  q no 7
+//  var flag = true;
+
+//  q n0 10
+
+// var userCity = prompt("enter your city");
+// var arr = ["karachi","lahore","islamabad","quetta", "kashmir","swat","gilgit","peshawar","muree"];
+// for(var i = 0; i<=arr.length; i++){
+// if(userCity === arr[i]){
+//     alert("enter")
+//     break;
+// }else{
+//     alert("Please write correct username")
+//     break;
+// }
+// }
+ 
+
+//  q no 8
+//   var pets = ["cat","dog","duck","elephant"];
+//   for(var i = 0; i<=pets.length ; i++){
+//     alert(i);
+//   }
+
+
+
+// q no 9
+// for(var i = 0; i<=10;i++){
+//     alert("it should be one")
+//     break;
+
+// }
+
+
+
+// q no 11
+// var userInput = prompt("enter value");
+// var list = ["laiba","zainab","hiba","faryal","maheen"];
+// matchFound = false;
+// for (var i = 0; i<=list.length; i++){
+//     if (userInput === list[i]) {
+//     matchFound = true;
+//      break;
+//     }
+//     }
+// if(matchFound === true){
+//     alert("Match found");
+// }else{
+//     alert("Match not found");
+// }
+
+
+
+
+
+
+// q no 12
+// var firstValue = ["a","b","c","d", " e"," f"];
+// var secondValue = [1,2,3,4,5,6];
+// for(var i=0; i<= firstValue.length; i++ ){
+//     for ( var j = 0; j<= secondValue.length; j++){
+//         console.log(firstValue[i]+ " " +secondValue[j])
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  2nd assignment
+// q no 3
+// for(var i=0;i<=10;i++){
+//     document.write(i , "<br>")
+// }
+// q no 4
+//  var userInput = +prompt("enter a value");
+//  var inputLength = +prompt("enter a lenght");
+// for(var i= 1; i<= inputLength;i++){
+// document.write(userInput + "*" + i + "=" + userInput  *  i + "<br>");
+// }
+// qno 5
+// fruits = ["apple", "banana", "mango", "orange",
+//     "strawberry"];
+//     console.log(fruits);
+//  q no 6
+// for(var i=1;i<=15;i++){
+//         document.write(i, " ");
+//     }
+
+
+// b
+// for(var i=10; i>=1; i--){
+//         document.write(i, " ");
+//     }
+
+// c
+// for(var i=0; i<=20; i+=2){
+//         document.write(i, " ");
+//     }
+//  d
+// for(var i=0; i<=20; i+=3){
+//         document.write(i, " ");
+//     }
+
+// e
+// for(var i=2; i<=20; i+=2){
+//         document.write(i+"k" + " ");
+//     }
+
+
+
+
+// Q 7
+// var userInput = prompt("enter");
+// var fruit = ["cake", "pine apple","cookie","chips"];
+// for(var i=0; i<=fruit.length; i++){
+//     if(userInput == fruit[1]){
+//         alert( userInput+ " "   + "is availale at index 1 in our bakery")
+//         break;
+//     }
+// else{
+//     alert( userInput+ " "   + "is not availale at index 3 in our bakery")
+// }
+   
+// }
+
+// q 8 or 9 skip
+
+// q no 10
+// for(i=1; i<=20; i++){
+//     document.write(5 * i + " ")
+// }
